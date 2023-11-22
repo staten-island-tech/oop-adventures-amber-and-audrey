b130e90c055bfe7aad5ec41a798183f7c3e0d12f
hambergss <achen200924@gmail.com>
2023-11-22 13:03:27 -0500

    194

--- OOPadventures copy.py
+++ OOPadventures copy.py
@@ -4,7 +4,29 @@ with open("enemies.json", "r") as f:
     # Serialize the updated Python list to a JSON string
     data = json.load(f)
 
-class enemies():
+class game():
+    def player(self, name, gender, element, weapon):
+        self.name = name
+        self.gender = gender
+        self.element = element
+        self.weapon = weapon
+        name = input("Enter your name: ")
+        gender = input("Enter your gender: ")
+        element = input("Enter your element: ")
+        weapon = input("Enter your weapon: ")
+        data.append(name,gender,element,weapon)
+
+    def __str__(self):
+        return f"{self.name}"
+    def __str__(self):
+        return f"{self.gender}"
+    def __str__(self):
+        return f"{self.element}"
+    def __str__(self):
+        return f"{self.weapon}"
+
+
+class enemies(game):
     def __init__(self, name, hp, atk, speed, defense):
         self.name = name
         self.hp = hp
@@ -23,3 +45,11 @@ class enemies():
     "speed": speed,
     "defense": defense
 }
+    data.append(base)
+
+new_file = "updated.json"
+with open(new_file, "w") as f:
+    json_string = json.dumps(data)
+    f.write(json_string)
+os.remove("enemies.json")
+os.rename(new_file, "enemies.json")
