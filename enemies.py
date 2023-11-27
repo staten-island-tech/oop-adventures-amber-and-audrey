0adb813fee3e9b3f6669ac381a0520eb273b430a
hambergss <achen200924@gmail.com>
2023-11-27 13:58:02 -0500

    ahhhhhhhhhhhhhhhhhhhhhhhhhhhhh

--- enemies.py
+++ enemies.py
@@ -44,16 +44,11 @@ class enemies(game):
     "defense": defense
 }
     
-    #No code needed below this line
-# Creates a new JSON file with the updated data
+
 new_file = "updated.json"
 with open(new_file, "w") as f:
-    # Serialize the updated Python list to a JSON string
     json_string = json.dumps(data)
-
-    # Write the JSON string to the new JSON file
     f.write(json_string)
 
-# Overwrite the old JSON file with the new one
 os.remove("enemies.json")
 os.rename(new_file, "enemies.json")
