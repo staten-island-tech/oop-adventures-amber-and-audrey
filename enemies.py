6599677ed0372a133c6f6aa5618acade3fb59c00
hambergss <achen200924@gmail.com>
2023-11-27 13:52:40 -0500

    THIS GUy IS GF A FOR RM FRA<S

---
+++ enemies.py
@@ -0,0 +1,59 @@
+import json 
+import os
+with open("enemies.json", "r") as f:
+    # Serialize the updated Python list to a JSON string
+    data = json.load(f)
+
+class game():
+    def player(self, name, gender, element, weapon):
+        self.name = name
+        self.gender = gender
+        self.element = element
+        self.weapon = weapon
+        name = input("Enter your name: ")
+        gender = input("Enter your gender: ")
+        element = input("Enter your element: ")
+        weapon = input("Enter your weapon: ")
+        data.append(name,gender,element,weapon)
+
+    def __str__(self):
+        return f"{self.name}"
+    def __str__(self):
+        return f"{self.gender}"
+    def __str__(self):
+        return f"{self.element}"
+    def __str__(self):
+        return f"{self.weapon}"
+class enemies(game):
+    def __init__(self, name, hp, atk, speed, defense):
+        self.name = name
+        self.hp = hp
+        self.atk = atk
+        self.speed = speed
+        self.defense = defense
+    name = input("Enter the enemies' name: ")
+    hp = int(input("Enter hp: "))
+    atk = int(input("Enter atk: "))
+    speed = int(input("Enter speed: "))
+    defense = int(input("Enter defense: "))
+    base = {
+    "name": name,
+    "hp": hp,
+    "atk": atk,
+    "speed": speed,
+    "defense": defense
+}
+    
+    #No code needed below this line
+# Creates a new JSON file with the updated data
+new_file = "updated.json"
+with open(new_file, "w") as f:
+    # Serialize the updated Python list to a JSON string
+    json_string = json.dumps(data)
+
+    # Write the JSON string to the new JSON file
+    f.write(json_string)
+
+# Overwrite the old JSON file with the new one
+os.remove("enemies.json")
+os.rename(new_file, "enemies.json")
