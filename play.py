96c98f5541ee25ceecbda1afb4a5daea6d13858a
hambergss <achen200924@gmail.com>
2023-12-01 13:59:01 -0500

    haha ambers sh

---
+++ play.py
@@ -0,0 +1,129 @@
+
+import json 
+import os
+
+print("Hello, player. Welcome to waifu simulator.")
+with open("game.json", "r") as f:
+    jsonLoad = json.load(f)
+name = 'o'
+gender = 'o'
+element = 'o'
+weapon = 'o'
+
+lvl = 1
+hp = 1
+atk = 1
+speed = 1
+defense = 1
+quest = 'o'
+
+class game():
+    def player(self, name, gender, element, weapon):
+        self.name = name
+        self.gender = gender
+        self.element = element
+        self.weapon = weapon
+    def __str__(self):
+        return f"{self.name}"
+    def __str__(self):
+        return f"{self.gender}"
+    def __str__(self):
+        return f"{self.element}"
+    def __str__(self):
+        return f"{self.weapon}"
+    
+    name = input("Enter your name: ")
+    gender = input("Enter your gender: ")
+    element = input("Enter your element: ")
+    weapon = input("Enter your weapon: ")
+    base = {
+        "name": name,
+        "gender": gender,
+        "element": element,
+        "weapon": weapon
+}
+
+    print("Hello", name)
+    if base == [name, gender, element, weapon]:
+        print("You are level 1 because you repel women lmao.")
+    jsonLoad.append(base)
+class level(game):
+    def createLevel(self, lvl, hp, atk, speed, defense, quest):
+        self.lvl = lvl
+        self.hp = hp
+        self.atk = atk
+        self.speed = speed
+        self.defense = defense
+        self.quest = quest
+    questList= []
+
+    firstmessage = input("would you like to complete the quest provided to level up? (Y/N)")
+    if firstmessage == ("Y"):
+        print(game.quest)
+
+
+class enemies(game):
+    def enemyCreate(self, name, hp, atk, speed, defense):
+        self.name = name
+        self.hp = hp
+        self.atk = atk
+        self.speed = speed
+        self.defense = defense
+    name = input("Enter the enemies' name: ")
+    hp = int(input("Enter hp: "))
+    atk = int(input("Enter atk: "))
+    speed = int(input("Enter speed: "))
+    defense = int(input("Enter defense: "))
+
+enemies = enemies()
+game = game()
+level = level()
+
+
+
+with open("game.json", "r") as f:
+    jsonLoad = json.load(f)
+
+    game.player(name, gender, element, weapon)
+    jsonLoad.append(game.base)
+
+new_file = "updated.json"
+with open(new_file, "w") as f:
+    json_string = json.dumps(jsonLoad)
+    f.write(json_string)
+
+os.remove("game.json")
+os.rename(new_file, "game.json")
+
+
+
+with open("level.json", "r") as f:
+    jsonLoad = json.load(f)
+
+    level.createLevel(lvl, hp, atk, speed, defense, quest)
+    jsonLoad.append(level.base)
+
+new_file = "updated.json"
+with open(new_file, "w") as f:
+    json_string = json.dumps(jsonLoad)
+    f.write(json_string)
+
+os.remove("level.json")
+os.rename(new_file, "level.json")
+
+
+
+with open("enemies.json", "r") as f:
+    jsonLoad = json.load(f)
+
+    enemies.enemyCreate(name, hp, atk, speed, defense)
+    jsonLoad.append(enemies.base)
+
+new_file = "updated.json"
+with open(new_file, "w") as f:
+    json_string = json.dumps(jsonLoad)
+    f.write(json_string)
+
+os.remove("enemies.json")
+os.rename(new_file, "enemies.json")
+
