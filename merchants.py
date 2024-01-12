fc6a4bc1a3c6126295b96f6cdfa1704c9217dc0a
hambergss <achen200924@gmail.com>
2024-01-12 14:00:54 -0500

    s

--- merchants.py
+++ merchants.py
@@ -12,7 +12,7 @@ class Merchants():
         print(products)
 
 
-Robert = Merchants("Robert", [])
+
 
 
 
--- prime.py
+++
@@ -1,8 +0,0 @@
-x = int(input("Enter a number: "))
-divisor = 1
-prime = [x]
-while divisor < x:
-    if x%divisor == 0:
-        divisor += 1
-    divisor += 1
-print(prime)
