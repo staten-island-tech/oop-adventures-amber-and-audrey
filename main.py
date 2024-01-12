7146177ce4a1f596b64d77a2572f2cf2e8b1783b
hambergss <achen200924@gmail.com>
2024-01-12 13:38:35 -0500

    Update main.py

--- main.py
+++ main.py
@@ -47,16 +47,16 @@ class level(user):
 
 
 
-lvl1 = level(1, 100, 5, 1, 0, ["Fight 3 slimes, Fight 3 hilichurls, Fight 1 bigchurls, Fight 2 abyss mages"])
-lvl2 = level(2, 120, 10, 5, 10, ["Fight 10 slimes, Fight 10 hilichurls, Fight 2 bigchurls, Fight 2 abyss mages"])
-lvl3 = level(3, 140, 20, 10, 20, ["Fight 15 slimes, Fight 15 Hilichurls, Fight 3 bigchurls, Fight 3 abyss mages, Fight 4 bandits"])
-lvl4 = level(4, 160, 30, 20, 30, ["Fight 25 slimes, Fight 25 hilichurls, Fight 4 bigchurls, Fight 4 abyss mages, Fight 5 bandits, Fight 1 samurai"])
-lvl5 = level(5, 180, 50, 30, 40, ["Fight 30 slimes, Fight 30 hilichurls, Fight 5 Bigchurls, Fight 5 Abyss Mages, Fight 8 bandits, Fight 2 samurai, fight 3 fatui agents"])
-lvl6 = level(6, 200, 80, 40, 50, ["Fight 46 slimes, Fight 46 hilichurls, Fight 7 Bighchurls, Fight 8 abyss mages, Fight 11 bandits, fight 4 samurai, Fight 4 fatui agents, fight 3 robots"])
-lvl7 = level(7, 220, 120, 50, 60, ["Fight 50 slimes, Fight 50 hilichurls, Fight 9 bigchurls, Fight 9 abyss mages, Fight 14 bandits, Fight 6 samurai, Fight 5 fatui agents, Fight 5 robots"])
-lvl8 = level(8, 240, 170, 60, 70, ["Fight 72 slimes, Fight 75 hilichurls, Fight 10 bigchurls, FIght 15 abyss mages, Fight 16 bandits, Fight 8 samurai, Fight 7 fatui agents, Fight 6 robots, Fight 3 natureswayatgettingbackatus"])
-lvl9 = level(9, 260, 230, 70, 80, ["Fight 80 slimes, Fight 80 hilichurls, Fight 15 bigchurls, Fight 20 abyss mages, Fight 10 samurai, Fight 10 fatui agents, Fight 10 robots, Fight 5 natureswayatgettingbackatus, Fight 1 bob"])
-lvl10 = level(10, 300, 300, 80, 90, ["Fight 100 slimes, Fight 100 hilichurls, Fight 20 Bigchurls, Fight 30 abyss mages, FIght 25 bandits, Fight 15 samurai, Fight 15 fatui agents, Fight 20 robots, Fight 6 natureswayatgettingbackatus, Fight 10 bobs"])
+lvl1 = level(1, 100, 5, 1, 0, ["Fight 1 slimes, Fight 1 hilichurls, Fight 1 bigchurls, Fight 1 abyss mages"])
+lvl2 = level(2, 120, 10, 5, 10, ["Fight 2 slimes, Fight 2 hilichurls, Fight 2 bigchurls, Fight 2 abyss mages"])
+lvl3 = level(3, 140, 20, 10, 20, ["Fight 3 slimes, Fight 3 Hilichurls, Fight 3 bigchurls, Fight 3 abyss mages, Fight 3 bandits"])
+lvl4 = level(4, 160, 30, 20, 30, ["Fight 4 slimes, Fight 4 hilichurls, Fight 4 bigchurls, Fight 4 abyss mages, Fight 4 bandits, Fight 4 samurai"])
+lvl5 = level(5, 180, 50, 30, 40, ["Fight 5 slimes, Fight 5 hilichurls, Fight 5 Bigchurls, Fight 5 Abyss Mages, Fight 5 bandits, Fight 5 samurai, fight 5 fatui agents"])
+lvl6 = level(6, 200, 80, 40, 50, ["Fight 6 slimes, Fight 6 hilichurls, Fight 6 Bighchurls, Fight 6 abyss mages, Fight 6 bandits, fight 6 samurai, Fight 6 fatui agents, fight 6 robots"])
+lvl7 = level(7, 220, 120, 50, 60, ["Fight 7 slimes, Fight 7 hilichurls, Fight 7 bigchurls, Fight 7 abyss mages, Fight 7 bandits, Fight 7 samurai, Fight 7 fatui agents, Fight 7 robots"])
+lvl8 = level(8, 240, 170, 60, 70, ["Fight 8 slimes, Fight 8 hilichurls, Fight 8 bigchurls, FIght 8 abyss mages, Fight 8 bandits, Fight 8 samurai, Fight 8 fatui agents, Fight 8 robots, Fight 8 natureswayatgettingbackatus"])
+lvl9 = level(9, 260, 230, 70, 80, ["Fight 9 slimes, Fight 9 hilichurls, Fight 9 bigchurls, Fight 9 abyss mages, Fight 9 samurai, Fight 9 fatui agents, Fight 9 robots, Fight 9 natureswayatgettingbackatus"])
+lvl10 = level(10, 300, 300, 80, 90, ["Fight 10 slimes, Fight 10 hilichurls, Fight 10 Bigchurls, Fight 10 abyss mages, Fight 10 bandits, Fight 10 samurai, Fight 10 fatui agents, Fight 10 robots, Fight 10 natureswayatgettingbackatus, Fight 1 bobs"])
 print(lvl1.hp)
 
 currentlvl = lvl1
